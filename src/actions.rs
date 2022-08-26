5dab88f96380cf2ff809b2e1d8e6cabd535ed396
p0p3yee <owner@kagome100.com>
2022-08-25 23:34:28 -0400

    Add update route & Fix bugs

--- src/actions.rs
+++ src/actions.rs
@@ -1,7 +1,7 @@
-use crate::models::{NewAccount, Account};
-use diesel::prelude::*;
+use crate::models::{NewAccount, Account, Endpoint};
+use diesel::{prelude::*, result::Error};
 
-pub fn create_account<'a>(conn: &SqliteConnection, addr: &'a str, name: &'a str, pkey: &'a str) {
+pub async fn create_account<'a>(conn: &SqliteConnection, addr: &'a str, name: &'a str, pkey: &'a str) -> Result<String, Error> {
     use crate::schema::accounts;
 
     let new_acc = NewAccount {
@@ -10,26 +10,36 @@ pub fn create_account<'a>(conn: &SqliteConnection, addr: &'a str, name: &'a str,
         private_key: pkey,
     };
 
-    diesel::insert_into(accounts::table)
+    let result = diesel::insert_into(accounts::table)
         .values(&new_acc)
-        .execute(conn)
-        .expect("Error creating account");
+        .execute(conn);
+    
+    match result {
+        Ok(_) => Ok(addr.to_string()),
+        Err(e) => Err(e),
+    }
 }
 
-// pub fn update_account_name(conn: &SqliteConnection, target_addr: &'a str, new_name: &'a str) {
-//     use crate::schema::accounts::dsl::*;
+pub async fn update_account_name(conn: &SqliteConnection, target_addr: &str, new_name: &str) -> Result<String, Error> {
+    use crate::schema::accounts::dsl::*;
 
-//     let acc = diesel::update(accounts.find(target_addr))
-//         .set(name.eq(new_name))
-//         .get_result::<Account>(conn)
-//         .expect(&format!("Target address not found"));
-// } 
-
-pub fn get_all_accounts(conn: &SqliteConnection) -> Result<Vec<Account>, diesel::result::Error> {
-    use crate::schema::accounts::dsl::accounts;
+    let result = diesel::update(accounts.find(target_addr))
+        .set(name.eq(new_name))
+        .filter(address.eq(target_addr))
+        .execute(conn);
     
-    let result = accounts
-        .load::<Account>(conn)?;
+    match result {
+        Ok(_) => Ok(new_name.to_string()),
+        Err(e) => Err(e)
+    }
+} 
 
-    Ok(result)
+pub async fn get_all_accounts(conn: &SqliteConnection) -> Result<Vec<Account>, diesel::result::Error> {
+    use crate::schema::accounts::dsl::accounts;
+    accounts.load::<Account>(conn)
+}
+
+pub async fn get_all_endpoints(conn: &SqliteConnection) -> Result<Vec<Endpoint>, diesel::result::Error> {
+    use crate::schema::endpoints::dsl::endpoints;
+    endpoints.load::<Endpoint>(conn)
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,21 +1,66 @@
-// use crate::models::{Endpoint, Account};
-
 use crate::actions::*;
-
+use crate::models::{ Account, Endpoint };
 use crate::Pool;
-use actix_web::{ web, get, HttpResponse, Error};
+use crate::apierror::ApiError;
+// use actix_web::{HttpRequest, Responder};
+use actix_web::{ web::{ self, Json }, get, post };
 
 #[get("/endpoint")]
-pub async fn get_endpoints(pool: web::Data<Pool>) -> Result<HttpResponse, Error> {
-    
-    Ok(HttpResponse::Ok().json(""))
+pub async fn get_endpoints(pool: web::Data<Pool>) -> Result<Json<Vec<Endpoint>>, ApiError> {
+    let db_conn = pool.get().unwrap();
+    match get_all_endpoints(&db_conn).await {
+        Ok(eps) => Ok(Json(eps)),
+        _ => Err(ApiError::DatabaseInternalError)
+    }
 }
 
 #[get("/account")]
-pub async fn get_accounts(pool: web::Data<Pool>) -> Result<HttpResponse, Error> {
+pub async fn get_accounts(pool: web::Data<Pool>) ->  Result<Json<Vec<Account>>, ApiError> {
     let db_conn = pool.get().unwrap();
-    Ok(match get_all_accounts(&db_conn) {
-        Ok(accs) => HttpResponse::Ok().json(accs),
-        _ => HttpResponse::from(HttpResponse::InternalServerError())
-    })
+    match get_all_accounts(&db_conn).await {
+        Ok(accs) => Ok(Json(accs)),
+        _ => Err(ApiError::DatabaseInternalError),
+    }
+}
+
+#[post("/account/new")]
+pub async fn new_account(acc: web::Json<Account>, pool: web::Data<Pool>) -> Result<Json<String>, ApiError> {
+    let db_conn = pool.get().unwrap();
+    let acc = acc.into_inner();
+    println!("New account: {:?}", acc);
+    let result = create_account(
+        &db_conn,
+        &acc.address,
+        &acc.name.unwrap_or_default(),
+        &acc.private_key.unwrap_or_default(),
+    ).await;
+
+    match result {
+        Ok (addr) => Ok(Json(addr.to_string())),
+        Err (e) => {
+            println!("Error in creating account: {:?}", e);
+            Err(ApiError::AccountCreationFailure)
+        }
+    }
+}
+
+#[post("/account/update")]
+pub async fn update_account(acc: web::Json<Account>, pool: web::Data<Pool>) -> Result<Json<String>, ApiError> {
+    let db_conn = pool.get().unwrap();
+    let acc = acc.into_inner();
+    let new_name = acc.name.unwrap_or_default();
+
+    let result = update_account_name(
+        &db_conn, 
+        &acc.address,
+        &new_name,
+    ).await;
+
+    match result {
+        Ok (name) => Ok(Json(name.to_string())),
+        Err (e) => {
+            println!("Error in updating account: {:?}", e);
+            Err(ApiError::AccountNotFound)
+        }
+    }
 }
